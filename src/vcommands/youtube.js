1f7aee230437478cdc20293c0964f02a3dbaea37
tomekb530 <tomekb530@gmail.com>
2020-03-17 13:08:03 +0100

    Youtube module polishing

--- src/vcommands/youtube.js
+++ src/vcommands/youtube.js
@@ -4,17 +4,19 @@ var wtn = require("words-to-numbers").wordsToNumbers
 
 module.exports = client=>{
     new client.VoiceCommand("play","Youtube stuff!",async (txt,connection,speaker)=>{
-        //connection.play(ytdl('https://www.youtube.com/watch?v=ZlAU_w7-Xp8', { quality: 'highestaudio' }));
         var r = await yts(txt)
         var url = r.videos[0].url
         if(url){
+            if(connection.song){
+                connection.song.end()
+                connection.song = undefined
+            }
             connection.song = connection.play(await ytdl(url), { type: 'opus' })
-            connection.song.setVolume(0.5)
+            connection.song.setVolume(0.1)
         }
     })
 
     new client.VoiceCommand("stop","Youtube stuff!",async (txt,connection,speaker)=>{
-        //connection.play(ytdl('https://www.youtube.com/watch?v=ZlAU_w7-Xp8', { quality: 'highestaudio' }));
         if(connection.song){
             connection.song.end()
             connection.song = undefined
@@ -22,10 +24,9 @@ module.exports = client=>{
     })
 
     new client.VoiceCommand("volume","Youtube stuff!",async (txt,connection,speaker)=>{
-        //connection.play(ytdl('https://www.youtube.com/watch?v=ZlAU_w7-Xp8', { quality: 'highestaudio' }));
         var volume = wtn(txt)
         if(connection.song && volume < 100){
-            connection.song.setVolume(volume/100)
+            connection.song.setVolume(volume/200)
         }
     })
 }
